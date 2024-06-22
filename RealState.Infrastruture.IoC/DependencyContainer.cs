0a90111cdb7a0dc162c99047d2dc0b46afca8e3f
Jorge Massi <j.massi2022@gmail.com>
2024-06-22 18:36:28 +0100

    update

--- RealState.Infrastruture.IoC/DependencyContainer.cs
+++ RealState.Infrastruture.IoC/DependencyContainer.cs
@@ -21,7 +21,7 @@ public static class DependencyContainer
 
             // Repos
 
-            services.AddScoped<PropertyRepository, PropertyRepository>();
+            services.AddScoped<IPropertyRepository, PropertyRepository>();
             services.AddScoped<DbContext, ApplicationDbContext>();
 
 
