73bd8800831e1a0748c9d22d4a4f6f5e2708bf6b
Massi <Massi@DESKTOP-A4S3PIK>
2024-09-23 16:47:36 +0100

    migration update

--- RealState.Infrastruture.Data/Migrations/20240923153403_RealEstate.Designer.cs
+++ RealState.Infrastruture.Data/Migrations/20240923153403_RealEstate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RealState.Infrastruture.Data.Repositories.ApplicationDbContexts;
 
@@ -11,9 +12,11 @@ using RealState.Infrastruture.Data.Repositories.ApplicationDbContexts;
 namespace RealState.Infrastruture.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240923153403_RealEstate")]
+    partial class RealEstate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
